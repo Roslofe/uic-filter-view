3169e60a9f0febaba33502e1023cfb4958ab24d9
Eli Roslöf <eli.roslof@gmail.com>
2024-12-15 12:23:04 +0200

    add styling to search results

--- src/components/ResultItem.vue
+++ src/components/ResultItem.vue
@@ -1,7 +1,28 @@
 <script setup>
 const props = defineProps(['result'])
+const name = props.result
 </script>
 
 <template>
-  <h1>Something</h1>
+  <button class="item-container">
+    <p>{{ name }}</p>
+  </button>
 </template>
+
+<style>
+.item-container {
+  box-sizing: border-box;
+  background-color: #e1cea9;
+  margin: 5px;
+  padding: 5px;
+  border-radius: 10px;
+  width: 95%;
+  max-width: 100%;
+  text-align: start;
+  transition: background-color 0.2s; /* some indication that the element has been pressed */
+}
+
+.item-container:active {
+  background-color: #aca28c;
+}
+</style>
--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -4,5 +4,15 @@ import ResultItem from './ResultItem.vue'
 const props = defineProps(['data'])
 </script>
 <template>
-  <ResultItem v-for="item in props.data" :result="item" v-bind:key="item.name" />
+  <div class="result-container">
+    <ResultItem v-for="item in props.data" :result="item.name" v-bind:key="item.name" />
+  </div>
 </template>
+
+<style>
+.result-container {
+  background-color: #daf3ff;
+  width: 100%;
+  border-radius: 5px;
+}
+</style>
